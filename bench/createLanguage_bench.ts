a23464730f7a777077901a414304b77908c0add8
Claudiu Ceia <claudiu.ceia+1@gmail.com>
2026-01-20 17:35:51 +0200

    chore: import type specifier

--- bench/createLanguage_bench.ts
+++ bench/createLanguage_bench.ts
@@ -1,6 +1,6 @@
 import { surrounded, either, seq, many, oneOf } from "../src/combinators.ts";
 import { createLanguage, type UntypedLanguage } from "../src/language.ts";
-import { Parser } from "../src/Parser.ts";
+import type { Parser } from "../src/Parser.ts";
 import { str, number } from "../src/parsers.ts";
 import { map, peekAnd, lazy } from "../src/utility.ts";
 
